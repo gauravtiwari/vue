ab4b8ad9d32312c26ae6a0f59ff6dd1a4d854431
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-29 18:47:30 +0300

    Fix indentation and minor spacing issues

--- examples/todomvc/js/app.js
+++ examples/todomvc/js/app.js
@@ -1,126 +1,137 @@
-/*global Vue, todoStorage */
+/* global Vue, todoStorage */
 
 (function (exports) {
+  'use strict';
 
-	'use strict';
+  var filters = {
+    all: function (todos) {
+      return todos;
+    },
 
-	var filters = {
-		all: function (todos) {
-			return todos;
-		},
-		active: function (todos) {
-			return todos.filter(function (todo) {
-				return !todo.completed;
-			});
-		},
-		completed: function (todos) {
-			return todos.filter(function (todo) {
-				return todo.completed;
-			});
-		}
-	};
+    active: function (todos) {
+      return todos.filter(function (todo) {
+        return !todo.completed;
+      });
+    },
 
-	exports.app = new Vue({
+    completed: function (todos) {
+      return todos.filter(function (todo) {
+        return todo.completed;
+      });
+    }
+  };
 
-		// the root element that will be compiled
-		el: '.todoapp',
+  exports.app = new Vue({
 
-		// app initial state
-		data: {
-			todos: todoStorage.fetch(),
-			newTodo: '',
-			editedTodo: null,
-			visibility: 'all'
-		},
+    // the root element that will be compiled
+    el: '.todoapp',
 
-		// watch todos change for localStorage persistence
-		watch: {
-			todos: {
-				handler: function (todos) {
-				  todoStorage.save(todos);
-				},
-				deep: true
-			}
-		},
+    // app initial state
+    data: {
+      todos: todoStorage.fetch(),
+      newTodo: '',
+      editedTodo: null,
+      visibility: 'all'
+    },
 
-		// computed properties
-		// http://vuejs.org/guide/computed.html
-		computed: {
-			filteredTodos: function () {
-				return filters[this.visibility](this.todos);
-			},
-			remaining: function () {
-				return filters.active(this.todos).length;
-			},
-			allDone: {
-				get: function () {
-					return this.remaining === 0;
-				},
-				set: function (value) {
-					this.todos.forEach(function (todo) {
-						todo.completed = value;
-					});
-				}
-			}
-		},
+    // watch todos change for localStorage persistence
+    watch: {
+      todos: {
+        handler: function (todos) {
+          todoStorage.save(todos);
+        },
+        deep: true
+      }
+    },
 
-		// methods that implement data logic.
-		// note there's no DOM manipulation here at all.
-		methods: {
+    // computed properties
+    // http://vuejs.org/guide/computed.html
+    computed: {
+      filteredTodos: function () {
+        return filters[this.visibility](this.todos);
+      },
 
-			addTodo: function () {
-				var value = this.newTodo && this.newTodo.trim();
-				if (!value) {
-					return;
-				}
-				this.todos.push({ title: value, completed: false });
-				this.newTodo = '';
-			},
+      remaining: function () {
+        return filters.active(this.todos).length;
+      },
 
-			removeTodo: function (todo) {
-				this.todos.$remove(todo);
-			},
+      allDone: {
+        get: function () {
+          return this.remaining === 0;
+        },
 
-			editTodo: function (todo) {
-				this.beforeEditCache = todo.title;
-				this.editedTodo = todo;
-			},
+        set: function (value) {
+          this.todos.forEach(function (todo) {
+            todo.completed = value;
+          });
+        }
+      }
+    },
 
-			doneEdit: function (todo) {
-				if (!this.editedTodo) {
-					return;
-				}
-				this.editedTodo = null;
-				todo.title = todo.title.trim();
-				if (!todo.title) {
-					this.removeTodo(todo);
-				}
-			},
+    // methods that implement data logic.
+    // note there's no DOM manipulation here at all.
+    methods: {
 
-			cancelEdit: function (todo) {
-				this.editedTodo = null;
-				todo.title = this.beforeEditCache;
-			},
+      addTodo: function () {
+        var value = this.newTodo && this.newTodo.trim();
+        if (!value) {
+          return;
+        }
 
-			removeCompleted: function () {
-				this.todos = filters.active(this.todos);
-			}
-		},
+        this.todos.push({
+          title: value,
+          completed: false
+        });
 
-		// a custom directive to wait for the DOM to be updated
-		// before focusing on the input field.
-		// http://vuejs.org/guide/custom-directive.html
-		directives: {
-			'todo-focus': function (value) {
-				if (!value) {
-					return;
-				}
-				var el = this.el;
-				Vue.nextTick(function () {
-					el.focus();
-				});
-			}
-		}
-	});
+        this.newTodo = '';
+      },
 
+      removeTodo: function (todo) {
+        this.todos.$remove(todo);
+      },
+
+      editTodo: function (todo) {
+        this.beforeEditCache = todo.title;
+        this.editedTodo = todo;
+      },
+
+      doneEdit: function (todo) {
+        if (!this.editedTodo) {
+          return;
+        }
+
+        this.editedTodo = null;
+        todo.title = todo.title.trim();
+
+        if (!todo.title) {
+          this.removeTodo(todo);
+        }
+      },
+
+      cancelEdit: function (todo) {
+        this.editedTodo = null;
+        todo.title = this.beforeEditCache;
+      },
+
+      removeCompleted: function () {
+        this.todos = filters.active(this.todos);
+      }
+    },
+
+    // a custom directive to wait for the DOM to be updated
+    // before focusing on the input field.
+    // http://vuejs.org/guide/custom-directive.html
+    directives: {
+      'todo-focus': function (value) {
+        if (!value) {
+          return;
+        }
+
+        var el = this.el;
+        Vue.nextTick(function () {
+          el.focus();
+        });
+      }
+    }
+  });
 })(window);
